975228f644b3af581c569c3f279d7a0ff596ccfe
Enes Aldemir <aldemirenes@live.com>
2020-04-27 16:07:51 +0200

    Add multipart attribute type

--- app/src/vls.js
+++ app/src/vls.js
@@ -153,6 +153,23 @@ export default {
                 "customShape": "<div class=\"custom_node\">\n<svg version=\"1.1\" viewBox=\"0.0 0.0 200.0 120.0\" fill=\"none\" stroke=\"none\" stroke-linecap=\"square\" stroke-miterlimit=\"10\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://www.w3.org/2000/svg\"><clipPath id=\"p.0\"><path d=\"m0 0l200.0 0l0 120.0l-200.0 0l0 -120.0z\" clip-rule=\"nonzero\"/></clipPath><g clip-path=\"url(#p.0)\"><path fill=\"#000000\" fill-opacity=\"0.0\" d=\"m0 0l200.0 0l0 120.0l-200.0 0z\" fill-rule=\"evenodd\"/><path fill=\"#dd7e6b\" d=\"m2.527559 0.5958005l194.49869 0l-97.249344 118.0z\" fill-rule=\"evenodd\"/><path stroke=\"#000000\" stroke-width=\"1.0\" stroke-linejoin=\"round\" stroke-linecap=\"butt\" d=\"m2.527559 0.5958005l194.49869 0l-97.249344 118.0z\" fill-rule=\"evenodd\"/><path fill=\"#000000\" fill-opacity=\"0.0\" d=\"m51.301838 14.060368l96.06299 0l0 46.834644l-96.06299 0z\" fill-rule=\"evenodd\"/><path fill=\"#000000\" d=\"m79.23177 34.108494l0 -3.234375l2.8125 0l0 3.234375l-2.8125 0zm0 19.671875l0 -16.59375l2.8125 0l0 16.59375l-2.8125 0zm5.96875 -4.953125l2.78125 -0.4375q0.234375 1.671875 1.296875 2.5625q1.078125 0.890625 3.0 0.890625q1.9375 0 2.875 -0.78125q0.9375 -0.796875 0.9375 -1.859375q0 -0.953125 -0.828125 -1.5q-0.578125 -0.375 -2.875 -0.953125q-3.09375 -0.78125 -4.296875 -1.34375q-1.1875 -0.578125 -1.8125 -1.578125q-0.609375 -1.015625 -0.609375 -2.234375q0 -1.109375 0.5 -2.046875q0.515625 -0.953125 1.390625 -1.578125q0.65625 -0.484375 1.78125 -0.8125q1.140625 -0.34375 2.4375 -0.34375q1.953125 0 3.421875 0.5625q1.484375 0.5625 2.1875 1.53125q0.703125 0.953125 0.96875 2.5625l-2.75 0.375q-0.1875 -1.28125 -1.09375 -2.0q-0.890625 -0.71875 -2.53125 -0.71875q-1.9375 0 -2.765625 0.640625q-0.828125 0.640625 -0.828125 1.5q0 0.546875 0.34375 0.984375q0.34375 0.453125 1.078125 0.75q0.421875 0.15625 2.484375 0.71875q2.984375 0.796875 4.15625 1.3125q1.1875 0.5 1.859375 1.46875q0.671875 0.96875 0.671875 2.40625q0 1.40625 -0.828125 2.65625q-0.8125 1.234375 -2.359375 1.921875q-1.546875 0.671875 -3.5 0.671875q-3.234375 0 -4.9375 -1.34375q-1.6875 -1.34375 -2.15625 -3.984375zm14.96875 4.953125l8.796875 -22.90625l3.265625 0l9.374992 22.90625l-3.453125 0l-2.671875 -6.9375l-9.578117 0l-2.515625 6.9375l-3.21875 0zm6.609375 -9.40625l7.765625 0l-2.390625 -6.34375q-1.09375 -2.890625 -1.625 -4.75q-0.4375 2.203125 -1.234375 4.375l-2.515625 6.71875z\" fill-rule=\"nonzero\"/></g></svg>\n<div class=\"fill_parent\">\n        <div style=\"position: absolute; left: 80%; top: 15%; -webkit-transform: translateY(-50%) translateX(-50%)\" class=\"name\"></div>\n    </div>\n</div>\n",
                 "customAnchors": ""
             }
+        },
+        "085ef30d15c32c4e89ca01ce": {
+            "label": "MultipartAttribute",
+            "attributes": {
+                "90d82a395d83f1da0603fb97": {
+                    "key": "Name",
+                    "value": "string"
+                }
+            },
+            "shape": {
+                "shape": "rounded_rectangle",
+                "color": "",
+                "defaultWidth": 100,
+                "defaultHeight": 60,
+                "customShape": "<div class=\"custom_node\">\n<svg version=\"1.1\" viewBox=\"0.0 0.0 200.0 120.0\" fill=\"none\" stroke=\"none\" stroke-linecap=\"square\" stroke-miterlimit=\"10\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://www.w3.org/2000/svg\"><clipPath id=\"p.0\"><path d=\"m0 0l200.0 0l0 120.0l-200.0 0l0 -120.0z\" clip-rule=\"nonzero\"/></clipPath><g clip-path=\"url(#p.0)\"><path fill=\"#000000\" fill-opacity=\"0.0\" d=\"m0 0l200.0 0l0 120.0l-200.0 0z\" fill-rule=\"evenodd\"/><path fill=\"#ffff00\" d=\"m7.905512 60.29659l0 0c0 -31.362818 41.58464 -56.787403 92.88189 -56.787403l0 0c51.297256 0 92.8819 25.424585 92.8819 56.787403l0 0c0 31.362812 -41.58464 56.787403 -92.8819 56.787403l0 0c-51.29725 0 -92.88189 -25.424591 -92.88189 -56.787403z\" fill-rule=\"evenodd\"/><path stroke=\"#000000\" stroke-width=\"1.0\" stroke-linejoin=\"round\" stroke-linecap=\"butt\" d=\"m7.905512 60.29659l0 0c0 -31.362818 41.58464 -56.787403 92.88189 -56.787403l0 0c51.297256 0 92.8819 25.424585 92.8819 56.787403l0 0c0 31.362812 -41.58464 56.787403 -92.8819 56.787403l0 0c-51.29725 0 -92.88189 -25.424591 -92.88189 -56.787403z\" fill-rule=\"evenodd\"/></g></svg>\n<div class=\"fill_parent\">\n        <div style=\"position: absolute; left: 50%; top: 50%; -webkit-transform: translateY(-50%) translateX(-50%)\" class=\"name\"></div>\n    </div>\n</div>\n",
+                "customAnchors": ""
+            }
         }
     },
     "edges": {
@@ -199,6 +216,15 @@ export default {
                     ],
                     "targetTypes": [
                         "WeakKeyAttribute"
+                    ]
+                },
+                {
+                    "sourceTypes": [
+                        "Entity",
+                        "WeakEntity"
+                    ],
+                    "targetTypes": [
+                        "MultipartAttribute"
                     ]
                 }
             ],
@@ -336,6 +362,28 @@ export default {
                     "position": "center"
                 }
             }
+        },
+        "52062150e816239baee7fbae": {
+            "label": "MultipartAttribute to Attribute",
+            "shape": {
+                "arrow": "bidirassociation",
+                "shape": "straight",
+                "color": "black",
+                "overlay": "",
+                "overlayPosition": "top",
+                "overlayRotate": true
+            },
+            "relations": [
+                {
+                    "sourceTypes": [
+                        "MultipartAttribute"
+                    ],
+                    "targetTypes": [
+                        "NonKeyAttribute"
+                    ]
+                }
+            ],
+            "attributes": {}
         }
     }
 }
